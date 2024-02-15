19fe8af05b7652ae6481e11e6a709fc5389bfb0a
Ju0011 <yhs07240@naver.com>
2024-02-15 10:37:50 +0900

    first commit

--- ws01_java/JavaEx/src/Main09_Thread.java
+++ ws01_java/JavaEx/src/Main09_Thread.java
@@ -1,4 +1,21 @@
 public class Main09_Thread {
+    public static void main(String[] args) throws Exception{
+        ThreadEx t1 = new ThreadEx();
+        ThreadEx t2 = new ThreadEx();
+        t1.start(); t2.start(); // run() 이 호출됨.
+        t1.join(); t2.join();
+        System.out.println(" 종료 ");
+    }
 }
-
-
+class ThreadEx extends Thread{  //또는  implements Runnable
+    public void run(){
+        for(int i=0 ; i < 1000 ; i++) {
+            System.out.println(i + getName());// Thread에 이름이 자동 할당되는데, 그 이름 출력.
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ ws01_java/JavaEx/src/Main10_hiding_overriding.java
@@ -0,0 +1,2 @@
+public class Main10_hiding_overriding {
+}
